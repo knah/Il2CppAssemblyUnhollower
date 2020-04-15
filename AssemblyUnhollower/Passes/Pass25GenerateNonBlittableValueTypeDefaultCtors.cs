9287db9567f33671fcdd1e9f43975b4aa6d27a4f
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-04-15 22:55:51 +0200

    Fix generation of default constructors for non-blittable value types

--- AssemblyUnhollower/Passes/Pass25GenerateNonBlittableValueTypeDefaultCtors.cs
+++ AssemblyUnhollower/Passes/Pass25GenerateNonBlittableValueTypeDefaultCtors.cs
@@ -21,14 +21,20 @@ namespace AssemblyUnhollower.Passes
                     
                     typeContext.NewType.Methods.Add(emptyCtor);
 
+                    var local0 = new VariableDefinition(assemblyContext.Imports.IntPtr);
+                    emptyCtor.Body.Variables.Add(local0);
+                    
                     var bodyBuilder = emptyCtor.Body.GetILProcessor();
                     bodyBuilder.Emit(OpCodes.Ldsfld, typeContext.ClassPointerFieldRef);
-                    bodyBuilder.Emit(OpCodes.Dup);
                     bodyBuilder.Emit(OpCodes.Ldc_I4_0);
                     bodyBuilder.Emit(OpCodes.Conv_U);
                     bodyBuilder.Emit(OpCodes.Call, assemblyContext.Imports.ValueSizeGet);
                     bodyBuilder.Emit(OpCodes.Conv_U);
                     bodyBuilder.Emit(OpCodes.Localloc);
+                    bodyBuilder.Emit(OpCodes.Stloc_0);
+                    bodyBuilder.Emit(OpCodes.Ldarg_0);
+                    bodyBuilder.Emit(OpCodes.Ldsfld, typeContext.ClassPointerFieldRef);
+                    bodyBuilder.Emit(OpCodes.Ldloc_0);
                     bodyBuilder.Emit(OpCodes.Call, assemblyContext.Imports.ObjectBox);
                     bodyBuilder.Emit(OpCodes.Call, new MethodReference(".ctor", assemblyContext.Imports.Void, typeContext.NewType.BaseType) { HasThis = true, Parameters = { new ParameterDefinition(assemblyContext.Imports.IntPtr) }});
                     bodyBuilder.Emit(OpCodes.Ret);
