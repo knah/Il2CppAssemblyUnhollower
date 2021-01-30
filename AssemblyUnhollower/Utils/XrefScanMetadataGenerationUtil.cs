d886d1debbdf9293045bfebdc2ec4b5237f6e624
Nikolay Kuznetsov <kna.rus@gmail.com>
2021-01-30 22:08:01 +0100

    Try different sources for metadata init call in xref scanner

--- AssemblyUnhollower/Utils/XrefScanMetadataGenerationUtil.cs
+++ AssemblyUnhollower/Utils/XrefScanMetadataGenerationUtil.cs
@@ -10,14 +10,29 @@ namespace UnhollowerRuntimeLib.XrefScans
         internal static long MetadataInitForMethodRva;
         internal static IntPtr MetadataInitForMethodFileOffset;
 
+        private static readonly (string Assembly, string Type, string Method)[] MetadataInitCandidates = {
+            ("UnityEngine.CoreModule", "UnityEngine.Object", ".cctor"),
+            ("mscorlib", "System.Exception", "get_Message"),
+            ("mscorlib", "System.IntPtr", "Equals")
+        };
+
         private static void FindMetadataInitForMethod(RewriteGlobalContext context, long gameAssemblyBase)
         {
-            var unityObjectCctor = context.Assemblies
-                .Single(it => it.OriginalAssembly.Name.Name == "UnityEngine.CoreModule").GetTypeByName("UnityEngine.Object").OriginalType.Methods.Single(it => it.Name == ".cctor");
+            foreach (var metadataInitCandidate in MetadataInitCandidates)
+            {
+                var assembly = context.Assemblies.FirstOrDefault(it => it.OriginalAssembly.Name.Name == metadataInitCandidate.Assembly);
+                var unityObjectCctor = assembly?.TryGetTypeByName(metadataInitCandidate.Type)?.OriginalType.Methods.FirstOrDefault(it => it.Name == metadataInitCandidate.Method);
+                
+                if(unityObjectCctor == null) continue;
+                
+                MetadataInitForMethodFileOffset =
+                    (IntPtr) ((long) XrefScannerLowLevel.JumpTargets((IntPtr) (gameAssemblyBase + unityObjectCctor.ExtractOffset())).First());
+                MetadataInitForMethodRva = (long) MetadataInitForMethodFileOffset - gameAssemblyBase - unityObjectCctor.ExtractOffset() + unityObjectCctor.ExtractRva();
 
-            MetadataInitForMethodFileOffset =
-                (IntPtr) ((long) XrefScannerLowLevel.JumpTargets((IntPtr) (gameAssemblyBase + unityObjectCctor.ExtractOffset())).First());
-            MetadataInitForMethodRva = (long) MetadataInitForMethodFileOffset - gameAssemblyBase - unityObjectCctor.ExtractOffset() + unityObjectCctor.ExtractRva();
+                return;
+            }
+
+            throw new ApplicationException("Unable to find a method with metadata init reference");
         }
 
         internal static (long FlagRva, long TokenRva) FindMetadataInitForMethod(MethodRewriteContext method, long gameAssemblyBase)
