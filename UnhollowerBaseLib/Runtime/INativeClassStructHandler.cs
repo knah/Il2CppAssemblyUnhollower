d641dd3b9674ab25b45216b1be094944745647fd
ghorsington <neigh@coder.horse>
2021-03-28 21:35:02 +0300

    Move struct wrappers to single class; add image wrappers
    (cherry picked from commit c41e57f510cd0122617298000124a543881fd6a9)

--- UnhollowerBaseLib/Runtime/INativeClassStructHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace UnhollowerBaseLib.Runtime
-{
-    public interface INativeClassStructHandler
-    {
-        INativeClassStruct CreateNewClassStruct(int vTableSlots);
-        unsafe INativeClassStruct Wrap(Il2CppClass* classPointer);
-    }
-}
--- UnhollowerBaseLib/Runtime/StructHandlerInterfaces.cs
+++ UnhollowerBaseLib/Runtime/StructHandlerInterfaces.cs
@@ -1,7 +1,13 @@
-using System;
+﻿using System;
 
 namespace UnhollowerBaseLib.Runtime
 {
+    public interface INativeClassStructHandler
+    {
+        INativeClassStruct CreateNewClassStruct(int vTableSlots);
+        unsafe INativeClassStruct Wrap(Il2CppClass* classPointer);
+    }
+
     public interface INativeClassStruct
     {
         IntPtr Pointer { get; }
@@ -16,19 +22,40 @@ namespace UnhollowerBaseLib.Runtime
         ref ClassBitfield1 Bitfield1 { get; }
         ref ClassBitfield2 Bitfield2 { get; }
         ref Il2CppClassAttributes Flags { get; }
-        
+
         ref IntPtr Name { get; }
         ref IntPtr Namespace { get; }
-        
+
         ref Il2CppTypeStruct ByValArg { get; }
         ref Il2CppTypeStruct ThisArg { get; }
-        
+
         unsafe ref Il2CppImage* Image { get; }
         unsafe ref Il2CppClass* Parent { get; }
         unsafe ref Il2CppClass* ElementClass { get; }
         unsafe ref Il2CppClass* CastClass { get; }
         unsafe ref Il2CppClass* Class { get; }
-        
+
         unsafe ref Il2CppMethodInfo** Methods { get; }
+    }
+
+    public interface INativeImageStructHandler
+    {
+        INativeImageStruct CreateNewImageStruct();
+        unsafe INativeImageStruct Wrap(Il2CppImage* classPointer);
+    }
+
+    public interface INativeImageStruct
+    {
+        IntPtr Pointer { get; }
+        
+        unsafe Il2CppImage* ImagePointer { get; }
+        
+        unsafe ref Il2CppAssembly* Assembly { get; }
+        
+        ref byte Dynamic { get; }
+        
+        ref IntPtr Name { get; }
+        
+        ref IntPtr NameNoExt { get; }
     }
 }
