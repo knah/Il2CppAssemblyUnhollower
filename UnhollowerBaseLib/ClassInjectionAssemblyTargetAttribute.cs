1a07c6463bf66fec22a412582d6ee5117726a1c9
ds5678 <jrpritts7@gmail.com>
2021-06-10 14:21:48 -0400

    Added an attribute for targeting assemblies in the class from name hook

---
+++ UnhollowerBaseLib/ClassInjectionAssemblyTargetAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnhollowerBaseLib;
+
+namespace UnhollowerRuntimeLib
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ClassInjectionAssemblyTargetAttribute : Attribute
+    {
+        string[] assemblies;
+
+        public ClassInjectionAssemblyTargetAttribute(string[] assemblies)
+        {
+            if (assemblies is null) this.assemblies = new string[0];
+            else this.assemblies = assemblies;
+        }
+        public ClassInjectionAssemblyTargetAttribute(string assembly)
+        {
+            if (string.IsNullOrWhiteSpace(assembly)) assemblies = new string[0];
+            else assemblies = new string[] { assembly };
+        }
+        internal IntPtr[] GetImagePointers()
+        {
+            List<IntPtr> result = new List<IntPtr>();
+            foreach (string assembly in assemblies)
+            {
+                IntPtr intPtr = IL2CPP.GetIl2CppImage(assembly);
+                if (intPtr != IntPtr.Zero) result.Add(intPtr);
+            }
+            return result.ToArray();
+        }
+    }
+}
