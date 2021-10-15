55cd146b2d1891ab5f26fa673ca5c4e1a2146c05
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 18:58:58 -0400

    add native type token attribute

---
+++ UnhollowerBaseLib/Attributes/NativeTypeTokenAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UnhollowerBaseLib.Attributes
+{
+	public class NativeTypeTokenAttribute : Attribute
+    {
+        public string AssemblyName;
+        public uint Token;
+    }
+}
