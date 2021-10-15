86d6b43c3206c37f8c951ee4bfcea0f0ab312723
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 18:51:47 -0400

    add native interfaces

---
+++ UnhollowerBaseLib/NativeTypes/IIl2CppObjectBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UnhollowerBaseLib
+{
+	public interface IIl2CppObjectBase
+	{
+		IntPtr Pointer { get; }
+		IntPtr PointerNullable { get; }
+		bool IsIl2CppObjectAlive();
+	}
+}
