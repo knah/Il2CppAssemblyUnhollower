3365a800ad19326abdd1e5067e32d0b795486522
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 21:41:03 -0400

    get type for class in runtime reflection helper

--- UnhollowerBaseLib/RuntimeReflectionHelper.cs
+++ UnhollowerBaseLib/RuntimeReflectionHelper.cs
@@ -16,6 +16,11 @@ namespace UnhollowerRuntimeLib
             return nestedType != null ? IL2CPP.il2cpp_class_from_system_type(nestedType.Pointer) : IntPtr.Zero;
         }
 
+        public static Type GetTypeForClass(IntPtr clazz)
+        {
+            return Type.internal_from_handle(IL2CPP.il2cpp_class_get_type(clazz));
+        }
+
         public static RuntimeTypeHandle GetRuntimeTypeHandle<T>()
         {
             return Il2CppType.Of<T>().TypeHandle;
