24e32ad27bad885f2bff6bdb38ab88efd50e3932
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 22:08:39 -0400

    expand il2cpp class pointer store

--- UnhollowerBaseLib/Il2CppClassPointerStore.cs
+++ UnhollowerBaseLib/Il2CppClassPointerStore.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Concurrent;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using UnhollowerBaseLib.Attributes;
+using UnhollowerRuntimeLib;
 
 namespace UnhollowerBaseLib
 {
@@ -9,10 +12,22 @@ namespace UnhollowerBaseLib
     {
         public static IntPtr NativeClassPtr;
         public static Type CreatedTypeRedirect;
-        
+
         static Il2CppClassPointerStore()
         {
             var targetType = typeof(T);
+            //NativeClassPtr = Il2CppClassPointerStore.GetClassPointerForType(targetType);//todo: uncomment
+            Il2CppClassPointerStore.InitializeForType(targetType);
+        }
+    }
+
+    public static class Il2CppClassPointerStore
+    {
+        private static readonly ConcurrentDictionary<Type, IntPtr> ourClassPointers = new();
+        private static readonly ConcurrentDictionary<Type, IntPtr> ourCreatedTypeRedirects = new();
+
+        internal static void InitializeForType(Type targetType)
+        {
             RuntimeHelpers.RunClassConstructor(targetType.TypeHandle);
             if (targetType.IsPrimitive || targetType == typeof(string))
             {
@@ -28,6 +43,49 @@ namespace UnhollowerBaseLib
                 var linkedType = (Type) customAttribute.ConstructorArguments[0].Value;
                 RuntimeHelpers.RunClassConstructor(linkedType.TypeHandle);
             }
+        }
+
+        public static void RegisterClassPointerForType(Type type, IntPtr clazz)
+        {
+            ourClassPointers[type] = clazz;
+        }
+
+        public static void RegisterTypeWithExplicitTokenInfo(Type type, string assemblyName, uint token)
+        {
+            ourClassPointers[type] = IL2CPP.GetClassPointerByToken(assemblyName, token, type);
+        }
+
+        /// <summary>
+        /// Don't call yet. Todo: apply NativeTypeTokenAttribute to assemblies
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IntPtr GetClassPointerForType(Type type)
+        {
+            return ourClassPointers.GetOrAdd(type, type =>
+            {
+                if (type.IsConstructedGenericType)
+                {
+                    var genericTypeDefinition = type.GetGenericTypeDefinition();
+                    if (genericTypeDefinition == typeof(Il2CppReferenceArray<>) || genericTypeDefinition == typeof(Il2CppStructArray<>))
+                    {
+                        var elementNativeType = RuntimeReflectionHelper.GetTypeForClass(GetClassPointerForType(type.GenericTypeArguments[0]));
+                        return IL2CPP.il2cpp_class_from_type(elementNativeType.MakeArrayType(1).TypeHandle.value);
+                    }
+
+                    var baseNativeType = GetClassPointerForType(genericTypeDefinition);
+                    return IL2CPP.il2cpp_array_class_get(baseNativeType, 1);
+                }
+
+                var tokenAttribute = (NativeTypeTokenAttribute)type.GetCustomAttribute(typeof(NativeTypeTokenAttribute));
+                if (tokenAttribute == null)
+                {
+                    LogSupport.Error($"Type {type} is not a valid type for use in IL2CPP (it's missing native token info)");
+                    return IntPtr.Zero;
+                }
+
+                return IL2CPP.GetClassPointerByToken(tokenAttribute.AssemblyName, tokenAttribute.Token, type);
+            });
         }
     }
 }
