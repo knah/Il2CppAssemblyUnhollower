32619347b053c2ea5451447e46494ca8a4181eed
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 22:50:50 -0400

    boxing and unboxing in il2cpp object base

--- UnhollowerBaseLib/NativeTypes/Il2CppObjectBase.cs
+++ UnhollowerBaseLib/NativeTypes/Il2CppObjectBase.cs
@@ -42,6 +42,9 @@ namespace UnhollowerBaseLib
             return TryCast<T>() ?? throw new InvalidCastException($"Can't cast object of type {Marshal.PtrToStringAnsi(IL2CPP.il2cpp_class_get_name(IL2CPP.il2cpp_object_get_class(Pointer)))} to type {typeof(T)}");
         }
 
+        /// <summary>
+        /// todo: replace with: `return this as T;` and make obsolete
+        /// </summary>
         public T TryCast<T>() where T : Il2CppObjectBase
         {
             var nestedTypeClassPointer = Il2CppClassPointerStore<T>.NativeClassPtr;
@@ -58,6 +61,13 @@ namespace UnhollowerBaseLib
             return (T)Activator.CreateInstance(Il2CppClassPointerStore<T>.CreatedTypeRedirect ?? typeof(T), Pointer);
         }
 
+        public static unsafe Il2CppObjectBase Box<T>(T valueType) where T : unmanaged
+        {
+            var nativeClass = Il2CppClassPointerStore<T>.NativeClassPtr;
+            if (nativeClass == IntPtr.Zero) throw new ArgumentException($"Type {typeof(T)} can't be represented in Il2Cpp domain");
+            return new Il2CppSystem.Object(IL2CPP.il2cpp_value_box(nativeClass, (IntPtr)(&valueType)));
+        }
+
         public T Unbox<T>() where T : unmanaged
         {
             var nestedTypeClassPointer = Il2CppClassPointerStore<T>.NativeClassPtr;
@@ -69,7 +79,30 @@ namespace UnhollowerBaseLib
                 throw new InvalidCastException($"Can't cast object of type {Marshal.PtrToStringAnsi(IL2CPP.il2cpp_class_get_name(IL2CPP.il2cpp_object_get_class(Pointer)))} to type {typeof(T)}");
 
             return Marshal.PtrToStructure<T>(IL2CPP.il2cpp_object_unbox(Pointer));
-        } 
+        }
+
+        public static unsafe Il2CppObjectBase BoxNonBlittable<T>(T valueType) where T : IIl2CppNonBlittableValueType
+        {
+            var nativeClass = Il2CppClassPointerStore<T>.NativeClassPtr;
+            if (nativeClass == IntPtr.Zero) throw new ArgumentException($"Type {typeof(T)} can't be represented in Il2Cpp domain");
+
+            fixed (void* bytes = valueType.ObjectBytes)
+                return new Il2CppSystem.Object(IL2CPP.il2cpp_value_box(nativeClass, (IntPtr)bytes));
+        }
+
+        public T UnboxNonBlittable<T>() where T : class, IIl2CppNonBlittableValueType
+        {
+            var nestedTypeClassPointer = Il2CppClassPointerStore<T>.NativeClassPtr;
+            if (nestedTypeClassPointer == IntPtr.Zero)
+                throw new ArgumentException($"{typeof(T)} is not an Il2Cpp reference type");
+
+            var ownClass = IL2CPP.il2cpp_object_get_class(Pointer);
+            if (!IL2CPP.il2cpp_class_is_assignable_from(nestedTypeClassPointer, ownClass))
+                throw new InvalidCastException($"Can't cast object of type {Marshal.PtrToStringAnsi(IL2CPP.il2cpp_class_get_name(IL2CPP.il2cpp_object_get_class(Pointer)))} to type {typeof(T)}");
+
+            //return GenericMarshallingUtils.MarshalObjectFromPointerKnownTypeBound<T>(Pointer);
+            throw new NotImplementedException();
+        }
 
         ~Il2CppObjectBase()
         {
