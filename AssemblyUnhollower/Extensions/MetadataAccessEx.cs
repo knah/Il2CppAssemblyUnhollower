e2ce09694b760d88d9d8a1ab1744861c724a2ee5
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-15 20:47:47 -0400

    metadata access extensions

---
+++ AssemblyUnhollower/Extensions/MetadataAccessEx.cs
@@ -0,0 +1,40 @@
+﻿using AssemblyUnhollower.MetadataAccess;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyUnhollower.Extensions
+{
+	public static class MetadataAccessEx
+	{
+        public static TypeDefinition? GetTypeByOriginalType(this IMetadataAccess metadataAccess, TypeDefinition originalType)
+        {
+            var typeFullName = originalType.GetFullNameWithNesting();
+            var typeAssemblyName = originalType.Module.Assembly.Name.Name;
+
+            return metadataAccess.GetTypeByName(typeAssemblyName, typeFullName);
+        }
+
+        public static TypeDefinition? GetTypeByOriginalTypeAnyAssembly(this IMetadataAccess metadataAccess, TypeDefinition originalType)
+        {
+            var typeFullName = originalType.GetFullNameWithNesting();
+
+            foreach (var metadataAccessAssembly in metadataAccess.Assemblies)
+            {
+                var result = metadataAccess.GetTypeByName(metadataAccessAssembly.Name.Name, typeFullName);
+                if (result != null) return result;
+            }
+
+            return null;
+        }
+
+        public static TypeDefinition? GetTypeByOriginalTypeOwnAssemblyFirst(this IMetadataAccess metadataAccess, TypeDefinition originalType)
+        {
+            return GetTypeByOriginalType(metadataAccess, originalType) ??
+                   GetTypeByOriginalTypeAnyAssembly(metadataAccess, originalType);
+        }
+    }
+}
